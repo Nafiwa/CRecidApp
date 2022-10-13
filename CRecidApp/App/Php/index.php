813fa564779f87c4da595705055fa9f1abd75df4
SofiaFlore <69054282+Nafiwa@users.noreply.github.com>
2022-10-13 13:30:45 -0500

    scale

--- CRecidApp/App/Php/index.php
+++ CRecidApp/App/Php/index.php
@@ -12,7 +12,8 @@ $con=mysqli_connect('localhost','root','','crecid');
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="../css/style.css" type="text/css" />
     <script type="text/javascript" src="../javaScript/javascript.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
